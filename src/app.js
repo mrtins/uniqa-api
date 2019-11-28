71cc39b8f35e87b84a2718d6582b1cbfad619794
Vitor Martins <vitor.f.martins@accenture.com>
2019-11-28 16:30:06 -0300

    Alterando estrutura do projeto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
 import cors from 'cors';
-import logger from 'morgan'
 import httpStatus from 'http-status';
 
 import APIError from './helpers/APIError';
-import router from './router';
+import indexRouter from './routes/index';
 
 const app = express();
 
@@ -13,9 +15,13 @@ app.use(logger('dev'));
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', router);
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
 
 app.use((req, res, next) => {
   const err = new APIError('API not found', httpStatus.NOT_FOUND);
